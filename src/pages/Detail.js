2cc9530cb2ff8d90fc600404d312c92aadcede5b
parksubin22 <117990474+parksubin22@users.noreply.github.com>
2022-12-19 09:40:07 +0900

    상품 상세 페이지 뼈대

---
+++ src/pages/Detail.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import DetailProductInfo from '../components/Detail/DetailProductInfo';
+import DetailComments from '../components/Detail/DetailComments';
+import styled from 'styled-components';
+
+const Detail = () => {
+    return (
+        <Wrap>
+            <Title>{/* <h1>상품 상세 페이지</h1> */}</Title>
+            <DetailProductInfo></DetailProductInfo>
+            <DetailComments></DetailComments>
+        </Wrap>
+    );
+};
+
+const Wrap = styled.div`
+    max-width: 1200px;
+    min-width: 800px;
+    height: 100%;
+    /* background-color: gray; */
+    /* border: 1px solid gray; */
+    margin: auto;
+    /* display: 'flex';
+    justify-content: center; */
+`;
+
+const Title = styled.div`
+    /* width: 100%; */
+    /* margin-top: 100px; */
+    margin-bottom: 30px;
+    height: 50px;
+    /* background-color: red; */
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export default Detail;
