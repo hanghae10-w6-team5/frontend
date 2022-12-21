f5b8923222efe3798c311c73152b08e01ef59e0d
soojung01 <ksj29622@naver.com>
2022-12-21 17:41:26 +0900

    feat: 로그인 페이지 api 수정

--- src/redux/lib/loginApi.js
+++ src/redux/lib/loginApi.js
@@ -1,12 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
+import axios from './core/axiosBaseInstance';
 
 export const __login = createAsyncThunk('login', async (payload, thunkAPI) => {
     try {
-        const res = await axios.post(
-            `https://dev-jn.shop/api/users/login`,
-            payload
-        );
+        const res = await axios.post(`/users/login`, payload);
         alert('로그인에 성공하였습니다!');
         localStorage.setItem('authentication', res.data.authentication);
         payload.navigate('/');
