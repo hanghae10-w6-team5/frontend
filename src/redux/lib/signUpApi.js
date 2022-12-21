612ebe135282c8a11438616f1f4c3b5b894a66bf
soojung01 <ksj29622@naver.com>
2022-12-22 02:39:13 +0900

    feat: 회원가입 페이지 수정

--- src/redux/lib/signUpApi.js
+++ src/redux/lib/signUpApi.js
@@ -5,14 +5,30 @@ export const __getValidId = createAsyncThunk(
     'getValidId',
     async (payload, thunkAPI) => {
         try {
-            await axios.get(`/users/signup/${payload.id}`, {
+            const res = await axios.get(`/users/signup/${payload.id}`, {
                 params: payload,
             });
             alert('사용 가능한 아이디 입니다!');
             payload.setIsUsableId(true);
+            return thunkAPI.fulfillWithValue(res);
         } catch (e) {
-            console.log(e);
-            alert('이미 사용 중인 id 입니다.');
+            alert(e.response.data.errorMessage);
+            return thunkAPI.rejectWithValue(e);
+        }
+    }
+);
+
+export const __signUp = createAsyncThunk(
+    'postsignUp',
+    async (payload, thunkAPI) => {
+        try {
+            const res = await axios.post(`/users/signup`, payload);
+            alert(res.data.message);
+            payload.navigate('/login');
+            return thunkAPI.fulfillWithValue(res);
+        } catch (e) {
+            alert(e.response.data.errorMessage);
+            return thunkAPI.rejectWithValue(e);
         }
     }
 );
